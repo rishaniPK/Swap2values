561420e92854cf5a896a65bb3ddaaa2be92bf5d8
Admin <Admin@DESKTOP-89NTJEN>
2019-05-22 11:10:25 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap2values
+{
+    class Program
+    {
+        //swaping Two Values 
+        static void Main(string[] args)
+        {
+            int num1, num2, swap; //Define variables
+            Console.WriteLine("Input the First Number : ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the Second Number : ");
+            num2 = int.Parse(Console.ReadLine());
+            swap = num1;
+            num1 = num2;
+            num2 = swap;
+            Console.WriteLine("After Swapping : ");
+            Console.WriteLine("First Number : " + num1);
+            Console.WriteLine("Second Number : " + num2);
+            Console.Read();
+        }
+    }
+}
